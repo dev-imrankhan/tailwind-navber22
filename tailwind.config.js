558992b38e5501d99ac52480c690a2288b36587e
Imran Khan <153602101+dev-imrankhan@users.noreply.github.com>
2024-07-04 11:01:04 +0600

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+tailwind.config ={
+    theam:{
+        extend:{
+            colors:{ 
+                 Clipboard:'#da373d',
+                Headerbg:'bg-cyan-500',
+            },
+            backgroundImage: {
+                'benner': "url('./benner.jpg')",
+               
+            }
+        }
+    }
+}
